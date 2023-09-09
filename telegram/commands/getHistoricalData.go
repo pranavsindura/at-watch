3623f570471ca1d5a239612abd2be311b72031c4
Pranav Sindura <pranav.sindura@gmail.com>
2023-09-09 12:16:08 +0530

    fetch multiple months data

--- telegram/commands/getHistoricalData.go
+++ telegram/commands/getHistoricalData.go
@@ -9,45 +9,59 @@ import (
 	"time"
 
 	telegramBot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jinzhu/copier"
 	fyersSDK "github.com/pranavsindura/at-watch/sdk/fyers"
 	"github.com/pranavsindura/at-watch/utils"
 	telegramUtils "github.com/pranavsindura/at-watch/utils/telegram"
 	"github.com/yukithm/json2csv"
 )
 
-func getHistoricalData(bot *telegramBot.BotAPI, update telegramBot.Update, instrument string, fromDate string, toDate string, resolution string) {
+func getHistoricalData(bot *telegramBot.BotAPI, update telegramBot.Update, instrument string, fromDateString string, toDateString string, resolution string) {
 	bot.Send(telegramUtils.GenerateReplyMessage(update, "Fetching Historical Data"))
 
-	from, _ := time.Parse(time.RFC3339, fromDate)
-	to, _ := time.Parse(time.RFC3339, toDate)
+	fromDate, _ := time.Parse(time.RFC3339, fromDateString)
+	toDate, _ := time.Parse(time.RFC3339, toDateString)
 
-	data, err := fyersSDK.FetchHistoricalData(instrument, resolution, from.Unix(), to.Unix(), 0)
-	if err != nil {
-		fmt.Println("Error while fetching historical data", err)
-		bot.Send(telegramUtils.GenerateReplyMessage(update, telegramUtils.GenerateGenericErrorText(fmt.Errorf("error while fetching historical data"))))
-		return
+	currentDate := time.Time{}
+	copier.CopyWithOption(&currentDate, &fromDate, copier.Option{DeepCopy: true})
+
+	rows := make([]json2csv.KeyValue, 0)
+
+	for currentDate.Sub(toDate).Milliseconds() <= 0 {
+		after3Months := currentDate.AddDate(0, 3, 0)
+		if toDate.Sub(after3Months).Milliseconds() < 0 {
+			after3Months = toDate
+		}
+
+		data, err := fyersSDK.FetchHistoricalData(instrument, resolution, currentDate.Unix(), after3Months.Unix(), 0)
+		if err != nil {
+			fmt.Println("Error while fetching historical data", err)
+			bot.Send(telegramUtils.GenerateReplyMessage(update, telegramUtils.GenerateGenericErrorText(fmt.Errorf("error while fetching historical data"))))
+			return
+		}
+
+		dataJSON := utils.BruteStringify(data)
+		var dataMapList []map[string]interface{}
+		json.Unmarshal([]byte(dataJSON), &dataMapList)
+
+		for _, tradesMap := range dataMapList {
+			row, err := json2csv.JSON2CSV(tradesMap)
+			if err != nil {
+				fmt.Println("attempting to convert to csv failed", row, err)
+			}
+			if len(row) != 1 {
+				fmt.Println("unexpected row length", row)
+				continue
+			}
+			rows = append(rows, row[0])
+		}
+
+		currentDate = after3Months.AddDate(0, 0, 1)
 	}
-
-	dataJSON := utils.BruteStringify(data)
-	var dataMapList []map[string]interface{}
-	json.Unmarshal([]byte(dataJSON), &dataMapList)
 
 	buff := &bytes.Buffer{}
 	csvWriter := json2csv.NewCSVWriter(buff)
 	csvWriter.HeaderStyle = json2csv.DotNotationStyle
-
-	rows := make([]json2csv.KeyValue, 0)
-	for _, tradesMap := range dataMapList {
-		row, err := json2csv.JSON2CSV(tradesMap)
-		if err != nil {
-			fmt.Println("attempting to convert to csv failed", row, err)
-		}
-		if len(row) != 1 {
-			fmt.Println("unexpected row length", row)
-			continue
-		}
-		rows = append(rows, row[0])
-	}
 
 	csvWriter.WriteCSV(rows)
 
